f8990e2e6000068eab12b1b133ed450667526ee5
nbegman <your_email@example.com>
2025-12-05 23:52:27 +1300

    Revert "Refactor Gameplay and Main modules: enhance player turn management and improve piece movement feedback"
    This reverts commit e8ec540717013a821ca9b59ada918016ae00bd96.

--- Gameplay.py
+++ Gameplay.py
@@ -12,7 +12,6 @@ class Gameplay:
         self.gameBoard = gameBoard
         self.badArmy = badArmy
         self.pieceToMove = None
-        self.playerTurn = None 
 
         
     def applyUpgrade(self, upgradeID, masterWindow):
@@ -86,13 +85,11 @@ class Gameplay:
     def playGame(self, m, w, rectangles, colours, selectedChoice):
 
         self.levelComplete = BooleanVar(value=False)
-        self.playerTurn = BooleanVar(value=True)
-        self.playerTurn.set(False)
         
         # Track the currently highlighted cell
         current_hover = {'cell': None}
 
-        # Handle click to move player piece
+        # Handle click to place player piece
         def on_mouse_click(event):
             col = event.x // self.gameBoard.cellWidth
             row = event.y // self.gameBoard.cellHeight
@@ -135,16 +132,14 @@ class Gameplay:
 
                 # Check if level is finished
                 if not self.badArmy or not self.playerArmy:
+                    
                     w.unbind('<Motion>')
                     w.unbind('<Button-1>')
                     self.applyUpgrade(selectedChoice.reward, m)
                     m.master.wait_variable(self.upgradeComplete)
                     m.destroy()
                     self.levelComplete.set(True)
-                else:
 
-                    # End player turn
-                    self.playerTurn.set(FALSE)
             else:
                 # Select piece to move if clicked on own piece
                 for piece in self.playerArmy:
@@ -189,30 +184,9 @@ class Gameplay:
                             w.itemconfig(prev_rect, fill=prev_color)
                             current_hover['cell'] = None
             
-        while True:
-            if self.playerTurn.get() == True:
-                w.bind('<Motion>', on_mouse_move)
-                w.bind('<Button-1>', on_mouse_click)          
-                w.wait_variable(self.playerTurn)
-            else:
-
-                # Stop player interaction
-                w.unbind('<Motion>')
-                w.unbind('<Button-1>')
-                
-                # Enemy turn logic here
-                for enemyPiece in self.badArmy:
-                    # implement simple AI
-                    pass
-
-                # End enemy turn
-                self.playerTurn.set(TRUE)
-
-
 
         w.bind('<Motion>', on_mouse_move)
-        w.bind('<Button-1>', on_mouse_click) 
-        self.playerTurn.set(TRUE)
+        w.bind('<Button-1>', on_mouse_click)
 
 
        
